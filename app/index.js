527eabe3b61c904f8edaf326129b6e43e234e18f
System Administrator <root@Adams-MacBook-Air.local>
2016-05-20 15:27:26 -0700

    (Feat) Signup creates storage folder for user's posts, posts are written to seperate files inside the user's folder, currently filenames are the blog posts' titles

--- app/index.js
+++ app/index.js
@@ -5,3 +5,5 @@ const port = process.env.PORT || 3333;
 app.listen(port);
 
 console.log('Server listening on port:',port);
+
+console.log(__dirname);
---
+++ app/server/data/api_handler/utils.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+
+module.exports = {
+
+	createUserFolder: function(req, res) {
+		const author = req.body.author;
+		let blogPostsPath = path.join(__dirname,'../blog_posts/');
+		fs.mkdir(path.join(blogPostsPath, author));
+
+	},
+
+	publish: function(req,res) {
+		const author = req.body.author;
+		const title = req.body.title;
+		const text = req.body.text;
+		const userPath = path.join(__dirname,'../blog_posts', author);
+		fs.writeFile(path.join(userPath, title + '.txt'), text, 'utf-8', function(err){
+			if (err) {
+				console.log(err)
+			}
+			else {
+				res.sendStatus(200);
+			}
+		});
+	}
+
+
+
+};
+	
+
+
