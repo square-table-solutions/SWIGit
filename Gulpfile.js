e70f5ec39fcc01432bf22716452ad95f4e5b617f
Sam Richards <samrrichards@gmail.com>
2016-05-26 14:19:00 -0700

    (fix) gulpfile now works exactly as expected

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,50 +1,47 @@
 const gulp = require('gulp'),
-gulpSequence = require('gulp-sequence'),
-gulpIgnore = require('gulp-ignore'),
-jshint = require('gulp-jshint'),
-cleanCSS = require('gulp-clean-css')
-concat = require('gulp-concat'),
-uglify = require('gulp-uglify'); 
+  gulpSequence = require('gulp-sequence'),
+  jshint = require('gulp-jshint'),
+  cleanCSS = require('gulp-clean-css')
+  concat = require('gulp-concat'),
+  babel = require('gulp-babel'); 
 
 const libs = './app/public/libs/'; 
 
-gulp.task('default', gulpSequence('concat', 
-  'lint', 
-  // 'minify-css'
-  ['minify-css', 'uglify']
-  )); 
-
-//For some reason, linting doesn't work if I pass in all our JS files.
-
-//I have it lint the concatenated file instead, which doesn't seem to hurt
-//the speed too much. 
+gulp.task('default', gulpSequence(['lint', 'minify-css'], 'concat_transpile')); 
 
 gulp.task('lint', function(){
   return gulp.src(
-  'app/public/build/js') //Takes in everything that's *not* in our public/lib folder 
+  ['app/server/**/*.js', 'app/public/js/**/*.js']) //Takes in everything that's *not* in our public/lib folder 
     .pipe(jshint())
 }); 
 
+//The gulp-babel module actually uglifies files *in addition* to transpling them. Since 
+//that's the case, and since the gulp-uglify module doesn't seem to play nice with files 
+//that haven't been transpiled, I've opted to use gulp-babel as our uglifier. 
 
-gulp.task('concat', function(){
-  return gulp.src(['./**/*.js']) //Takes in all our JavaScript files 
-  .pipe(concat('app.js'))
-  // .pipe(gulpIgnore).exclude('node_modules')
-  // .pipe(gulpIgnore).exclude('gulpfile.js')
-  .pipe(gulpIgnore.exclude('app/public/libs/angular')) //Excludes our CRM libraries 
-  .pipe(gulpIgnore.exclude('app/public/libs/jquery'))
-  .pipe(gulpIgnore.exclude('app/public/libs/materialize'))
+//Also, I've found that chaining transpilation to concatenation is the most efficient way
+//to get a single minified js file, so I've gone ahead and combined those tasks. 
+
+
+gulp.task('concat_transpile', function(){
+  return gulp.src(['app/public/js/**/*.js', 
+   'app/public/libs/angular-materialize/**/*.js',
+   'app/public/libs/angular-ui-router/**/*.js']) //Takes in all our JavaScript files 
+  .pipe(concat('app-min.js'))
+  .pipe(babel({
+    presets: ['es2015']
+  }))
   .pipe(gulp.dest('app/public/build/js')); 
 }); 
 
-//NOTE: Concatenation *technically* seems to work, but takes about 6-7 seconds and produces a much 
-//larger file than we'd probably like (~60 MB in size!), which Sublime Text can't load (but TextEdit
-//can for some reason). The file doesn't appear to be ugified even though gulp claims that uglification
-//worked and doesn't seem to exclude some of the libraries we're trying to exclude (like jQuery). 
 
-//Also, concat throws an error ("dest.on is not a function") if I try excluding either
-//the node_modules folder or the gulpfile itself. It looks like most of the concatenated file is
-//comprised of node modules, so we'll probably want to fix this ASAP. 
+gulp.task('transpile', function(){
+  return gulp.src('app/public/build/js/*.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('app/public/build/js/dist'))
+});
 
 gulp.task('minify-css', function(){
   return gulp.src('app/public/css/layout.css')
@@ -53,11 +50,3 @@ gulp.task('minify-css', function(){
     }))
       .pipe(gulp.dest('app/public/build/css/')); 
 }); 
-
-//NOTE: For some reason, uglification throws an error if I try piping to a app-min.js file. 
-
-gulp.task('uglify', function(){
-  return gulp.src('app/public/build/js/app.js'); 
-    // .pipe(uglify('app/public/build/js/app-min.js'))
-    // .pipe(gulp.dest('app/public/build/js')); 
-});
